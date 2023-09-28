3f74b2d8f1d40fc80499c0e1da95b5fe02a46859
unknown <jonas.gastro91@gmail.com>
2023-09-28 16:07:22 -0300

    feat: cpf validator and test before clean code

---
+++ backend/ride/test/cpfValidator.test.ts
@@ -0,0 +1,22 @@
+import { validate } from "../src/CpfValidator";
+
+test.each([
+  "83432616074",
+  "87175659520",
+  "74587887803"
+])("Deve testar os cpfs validos", function (cpf:string) {
+  const isValid = validate(cpf);
+  expect(isValid).toBeTruthy();
+});
+
+test.each([
+  "83432616076",
+  "99999999999",
+  "834326160",
+  "",
+  null,
+  undefined
+])("Deve testar os cpfs invalidos", function (cpf:any) {
+  const isValid = validate(cpf);
+  expect(isValid).toBeFalsy();
+});
