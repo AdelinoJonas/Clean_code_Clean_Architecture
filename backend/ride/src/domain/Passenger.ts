e6db21c4c48be2aa61d7d942996c3245b8a2229e
unknown <jonas.gastro91@gmail.com>
2024-03-05 17:56:39 -0300

    feat: email domain

--- backend/ride/src/domain/Passenger.ts
+++ backend/ride/src/domain/Passenger.ts
@@ -1,7 +1,13 @@
+import Cpf from "./Cpf";
+import Email from "./Email";
+
 export default class Passenger {
+  document: Cpf;
+  email: Email;
 
-  constructor (readonly name: string, readonly email: string, readonly document: string) {
-
+  constructor (readonly name: string, email: string, document: string) {
+    this.document = new Cpf(document);
+    this.email = new Email(email);
   }
 
   static create (name: string, email: string, document: string) {
--- backend/ride/test/unit/Passenger.test.ts
+++ backend/ride/test/unit/Passenger.test.ts
@@ -1,8 +1,15 @@
 import Passenger from "../../src/domain/Passenger";
 
 test("Deve criar um passageiro", function () {
-   const passenger = new Passenger("Jhon Doe","jhon.doe@gmail","83432616074");
+   const passenger = Passenger.create("Jhon Doe","jhon.doe@gmail.com","83432616074");
    expect(passenger.name).toBe("Jhon Doe");
-   expect(passenger.email).toBe("jhon.doe@gmail");
-   expect(passenger.document).toBe("83432616074");
+   expect(passenger.email.value).toBe("jhon.doe@gmail.com");
+   expect(passenger.document.value).toBe("83432616074");
+});
+test("Não pode criar um passageiro com cpf inválido", function () {
+   expect(() => Passenger.create("Jhon Doe","jhon.doe@gmail.com","83432616076")).toThrow(new Error("Invalid cpf"));
+});
+
+test("Não pode criar um passageiro com email inválido", function () {
+   expect(() => Passenger.create("Jhon Doe","jhon.doe@","83432616074")).toThrow(new Error("Invalid email"));
 });
