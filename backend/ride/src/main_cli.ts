c8c420e33469c05c6603345d1d45ee07b81cf68c
jonas <adelino.jonas.dev@gmail.com>
2024-06-14 19:14:04 -0300

    feat: main_cli e main api_

--- backend/ride/src/main_cli.ts
+++ backend/ride/src/main_cli.ts
@@ -7,17 +7,3 @@ const passengerRepository = new PassengerRepositoryDataBase();
 const createPassenger = new CreatePassenger(passengerRepository);
 const inputOutput = new NodeInputOutput();
 new CLIController(inputOutput, createPassenger);
-
-// process.stdin.on("data", async function(chunk){
-//   const command = chunk.toString().replace(/\n/g, "");
-//   try {
-//     if (command.startsWith("create-passenger")) {
-//       const [ name, email, document ] = command.replace("create-passenger","").split("");
-//       const usecase = new CreatePassenger(new PassengerRepositoryDataBase());
-//       await usecase.execute({name, email, document});
-//     }
-//   } catch (e:any) {
-//     console.log(e.message);
-//   }
-// });
-
