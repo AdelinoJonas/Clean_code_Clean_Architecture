5ed3460d4d5fe41c8f2bf8d0366de612b3f3f40a
unknown <jonas.gastro91@gmail.com>
2024-06-19 12:30:36 -0300

    refactor: calculate RIde

--- backend/ride/test/integration/api.test.ts
+++ backend/ride/test/integration/api.test.ts
@@ -6,13 +6,26 @@ axios.defaults.validateStatus = function() {
 
 test("Deve fazer o cálculo do preço de uma corrida durante o dia", async function(){
   const input = {
-    segments: [
-    {distance: 10, date: "2021-03-01T10:00:00"}
+    positions : [
+      {lat: -27.584905257808835, long: -48.545022195325124, date: new Date("2021-03-01T10:00:00")},
+      {lat: -27.496887588317275, long: -48.522234807851475, date: new Date("2021-03-01T10:00:00")}
     ]
   };
   const response = await axios.post("http://localhost:3000/calculate_ride", input);
   const output = response.data;
   expect(output.price).toBe(21);
+});
+test("Deve retornar erro se a data for inválida", async function(){
+  const input = {
+    positions : [
+      {lat: -27.584905257808835, long: -48.545022195325124, date: "javascript"},
+      {lat: -27.496887588317275, long: -48.522234807851475, date: "javascript"}
+    ]
+  };
+  const response = await axios.post("http://localhost:3000/calculate_ride", input);
+  expect(response.status).toBe(422);
+  const output = response.data;
+  expect(output).toBe("Invalid date");
 });
 
 test("Deve cadastrar um passageiro", async function(){
