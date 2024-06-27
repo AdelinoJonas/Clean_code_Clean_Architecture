690f629b796fc15abeac156907e73f6750d1ccce
unknown <jonas.gastro91@gmail.com>
2024-06-27 11:33:36 -0300

    cors - backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,20 +14,28 @@ import axios from 'axios';
       document: document.value
     };
     const response = await axios.post("http://localhost:3000/passengers", input);
-    const output = response.data;
-    console.log(output)
-    passengerId.value = output.passengerId;
+    const output = response.data.passenger_id;
+    passengerId.value = output;
   }
 </script>
 
 <template>
-  <div>
-    <input class="passenger-name" v-model="name"/>
-    <input class="passenger-email" v-model="email"/>
-    <input class="passenger-document" v-model="document"/>
+  <form class="passenger-register">
+    <div class="field">
+      <label for="" class="label">Name:</label>
+      <input class="passenger-name" v-model="name"/>
+    </div>
+    <div class="field">
+      <label for="" class="label">Email:</label>
+      <input class="passenger-email" v-model="email"/>
+    </div>
+    <div class="field">
+      <label for="" class="label">Document:</label>
+      <input class="passenger-document" v-model="document"/>
+    </div>
     <button class="create-passenger-button" @click="createPassenger()">Create Passenger</button>
-    <div class="passenger-id">{{ passengerId }}</div>
-  </div>
+    <div class="passenger-id">{{passengerId}}</div>
+  </form>
 </template>
 
 <style scoped>
