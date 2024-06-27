d2d49c8ddab7ccd9bfc4f698af8259b8083a01e2
unknown <jonas.gastro91@gmail.com>
2024-06-26 21:03:05 -0300

    feat:create-passenger

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import axios from 'axios';
+
+  const name = ref("");
+  const email = ref("");
+  const document = ref("");
+  const passengerId = ref("");
+
+  async function createPassenger () {
+    const input = {
+      name: name.value,
+      email: email.value,
+      document: document.value
+    };
+    const response = await axios.post("http://localhost:3000/passengers", input);
+    const output = response.data;
+    console.log(output)
+    passengerId.value = output.passengerId;
+  }
 </script>
 
 <template>
-  <div class="test">
-    Teste de aplicação
+  <div>
+    <input class="passenger-name" v-model="name"/>
+    <input class="passenger-email" v-model="email"/>
+    <input class="passenger-document" v-model="document"/>
+    <button class="create-passenger-button" @click="createPassenger()">Create Passenger</button>
+    <div class="passenger-id">{{ passengerId }}</div>
   </div>
 </template>
 
--- frontend/test/app.test.ts
+++ frontend/test/app.test.ts
@@ -1,7 +1,23 @@
 import { mount } from "@vue/test-utils";
 import AppVue from "../src/App.vue"
 
-test("Deve testar a aplicação", function (){
+function sleep (time: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  })
+};
+test("Cadastrar um passageiro", async function (){
   const wrapper = mount(AppVue, {});
-  expect(wrapper.get(".test").text()).toBe("Teste de aplicação");
+  await wrapper.get(".passenger-name").setValue("Ana");
+  await wrapper.get(".passenger-email").setValue("Ana@gmail.com");
+  await wrapper.get(".passenger-document").setValue("83432616074");
+  await wrapper.get(".create-passenger-button").trigger("click");
+  await sleep(200);
+  let passengerId = "0";
+  if(wrapper.get(".passenger-id").text() > "0") {
+    return;
+  } 
+  expect(passengerId).toBeDefined();
 })
